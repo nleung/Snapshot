3a51f3a91925b2bf39e94c3e9fc1fea39675669d
Nathan Leung <leungn@gmail.com>
2012-04-06 00:06:17 -0700

    Move all Facebook stuff to its own activity.  Also add user info lookup.

--- mainline/src/com/nleung/snapshot/HomeActivity.java
+++ mainline/src/com/nleung/snapshot/HomeActivity.java
@@ -1,108 +1,23 @@
 package com.nleung.snapshot;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import com.facebook.android.AsyncFacebookRunner;
-import com.facebook.android.AsyncFacebookRunner.RequestListener;
-import com.facebook.android.Facebook;
-import com.facebook.android.FacebookError;
-import com.nleung.snapshot.facebook.FacebookConstants;
-import com.nleung.snapshot.facebook.FacebookSSO;
-import com.nleung.snapshot.facebook.FacebookSSOActivity;
+import com.nleung.snapshot.facebook.FacebookActivity;
 
 public class HomeActivity extends Activity 
 {
-    private Facebook facebook = new Facebook(FacebookConstants.APP_ID);
-    FacebookSSO sso;
-
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        
-        this.sso = new FacebookSSO(this);
-        
-        refreshFacebookButtons();
+        setContentView(R.layout.home);        
     }
     
-    @Override
-    public void onResume()
+    public void onFacebookClick(View view)
     {
-        super.onResume();
-        
-        refreshFacebookButtons();
-    }
-    
-    private void refreshFacebookButtons()
-    {
-        Button loginButton = (Button) findViewById(R.id.facebook_login_button);
-        Button logoutButton = (Button) findViewById(R.id.facebook_logout_button);
-
-        if (this.sso.isAuthenticated(this.facebook))
-        {
-            loginButton.setText("Already logged in via Facebook");
-            loginButton.setEnabled(false);
-            
-            logoutButton.setText("Logout of Facebook");
-            logoutButton.setEnabled(true);
-        }
-        else
-        {
-            loginButton.setText("Login to Facebook");
-            loginButton.setEnabled(true);
-            
-            logoutButton.setText("Not logged into Facebook");
-            logoutButton.setEnabled(false);
-        }
-    }
-    
-    public void onFacebookLoginClick(View view)
-    {
-    	startActivity(new Intent(this, FacebookSSOActivity.class));
-    }
-    
-    public void onFacebookLogoutClick(View view)
-    {
-        
-        AsyncFacebookRunner runner = new AsyncFacebookRunner(this.facebook);
-        runner.logout(this, new RequestListener()
-        {
-            @Override
-            public void onComplete(String response, Object state)
-            {
-                HomeActivity.this.sso.logout();
-                runOnUiThread(new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        refreshFacebookButtons();
-                    }
-                });
-            }
-            
-            @Override
-            public void onIOException(IOException e, Object state) {}
-            
-            @Override
-            public void onFileNotFoundException(FileNotFoundException e,
-                  Object state) {}
-            
-            @Override
-            public void onMalformedURLException(MalformedURLException e,
-                  Object state) {}
-            
-            @Override
-            public void onFacebookError(FacebookError e, Object state) {}
-          });
+    	startActivity(new Intent(this, FacebookActivity.class));
     }
 }
---
+++ mainline/src/com/nleung/snapshot/facebook/FacebookActivity.java
@@ -0,0 +1,218 @@
+package com.nleung.snapshot.facebook;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+import com.nleung.snapshot.R;
+
+public class FacebookActivity extends Activity
+{
+    Facebook facebook = new Facebook(FacebookConstants.APP_ID);
+    private FacebookSSO sso;
+    private AsyncFacebookRunner runner;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_sso);
+        
+        this.sso = new FacebookSSO(this);
+        this.runner = new AsyncFacebookRunner(this.facebook);
+        refreshFacebookInfo();
+    }
+    
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        
+        refreshFacebookInfo();
+    }
+    
+    private void refreshFacebookInfo()
+    {
+        Button loginButton = (Button) findViewById(R.id.facebook_login_button);
+        Button logoutButton = (Button) findViewById(R.id.facebook_logout_button);
+
+        if (this.sso.isAuthenticated(this.facebook))
+        {
+            loginButton.setText("Already logged in via Facebook");
+            loginButton.setEnabled(false);
+            
+            logoutButton.setText("Logout of Facebook");
+            logoutButton.setEnabled(true);
+        }
+        else
+        {
+            loginButton.setText("Login to Facebook");
+            loginButton.setEnabled(true);
+            
+            logoutButton.setText("Not logged into Facebook");
+            logoutButton.setEnabled(false);
+        }
+        
+        getUserInfo();
+    }
+    
+    public void onFacebookLoginClick(View view)
+    {
+        authorize();
+    }
+    
+    private void authorize()
+    {
+        if (!this.sso.isAuthenticated(this.facebook))
+        {
+            this.facebook.authorize(this, FacebookConstants.PERMISSIONS, new DialogListener()
+            {
+                @Override
+                public void onComplete(Bundle values)
+                {
+                    FacebookActivity.this.sso.setAccessToken(FacebookActivity.this.facebook.getAccessToken());
+                    FacebookActivity.this.sso.setAccessExpires(FacebookActivity.this.facebook.getAccessExpires());
+                }
+
+                @Override
+                public void onFacebookError(FacebookError error) {}
+
+                @Override
+                public void onError(DialogError e) {}
+
+                @Override
+                public void onCancel() {}
+            });
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        this.facebook.authorizeCallback(requestCode, resultCode, data);
+    }
+    
+    public void onFacebookLogoutClick(View view)
+    {
+        runner.logout(this, new RequestListener()
+        {
+            @Override
+            public void onComplete(String response, Object state)
+            {
+                FacebookActivity.this.sso.logout();
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        refreshFacebookInfo();
+                    }
+                });
+            }
+            
+            @Override
+            public void onIOException(IOException e, Object state) {}
+            
+            @Override
+            public void onFileNotFoundException(FileNotFoundException e,
+                  Object state) {}
+            
+            @Override
+            public void onMalformedURLException(MalformedURLException e,
+                  Object state) {}
+            
+            @Override
+            public void onFacebookError(FacebookError e, Object state) {}
+          });
+    }
+    
+    private void getUserInfo()
+    {
+        this.runner.request("me", new RequestListener()
+        {
+            
+            @Override
+            public void onMalformedURLException(MalformedURLException e, Object state)
+            {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void onIOException(IOException e, Object state)
+            {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void onFileNotFoundException(FileNotFoundException e, Object state)
+            {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void onFacebookError(FacebookError e, Object state)
+            {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void onComplete(String response, Object state)
+            {
+                Log.d(getLocalClassName(), "response is: " + response);
+                
+                final StringBuilder builder = new StringBuilder();
+                try
+                {
+                    JSONObject responseJson = new JSONObject(response);
+                    Iterator it = responseJson.keys();
+                    while (it.hasNext())
+                    {
+                        String key = (String) it.next();
+                        String value = responseJson.getString(key);
+                        
+                        builder.append(key).append("=").append(value).append("\n");
+                    }
+                }
+                catch (JSONException e)
+                {
+                    Log.e(getLocalClassName(), e.toString());
+                }
+                
+                FacebookActivity.this.runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        TextView userInfo = (TextView) findViewById(R.id.facebook_user_info);
+                        userInfo.setText(builder.toString());
+                    }
+                });
+            }
+        });
+    }
+}
